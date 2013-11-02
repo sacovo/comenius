7625b32a16412f926d414d536a79e91461d8eef8
Sandro Covo <sandro@covo.ch>
2013-11-02 17:23:24 +0100

    Add login to navigation

--- urls.py
+++ urls.py
@@ -3,3 +3,7 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('comenius.views',
     url(r"^$", 'index', name="index"),
 )
+
+urlpatterns += patterns('django.contrib.auth.views',
+    url(r"^accounts/login/$", 'login', name="login"),
+)
