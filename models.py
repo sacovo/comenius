b0e69a32e22964812bbd6d550ba147090c2ff84f
Sandro Covo <sandro@covo.ch>
2013-11-09 20:52:22 +0100

    Add first model

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Album(models.Model):
+    name = models.CharField(max_length=40)
+    images = models.ManyToManyField("Image")
+    owner = models.ForeignKey(User)
+
+class Image(models.Model):
+    pass
