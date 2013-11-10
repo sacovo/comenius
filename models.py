f898e6d1bac5a37474cf33442a7f11c72a086137
Sandro Covo <sandro@covo.ch>
2013-11-10 12:32:56 +0100

    Add album

--- models.py
+++ models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 
 class Album(models.Model):
     name = models.CharField(max_length=40)
-    images = models.ManyToManyField("Image")
+    images = models.ManyToManyField("Image", blank=True)
     owner = models.ForeignKey(User)
 
+
 class Image(models.Model):
-    pass
+    image = models.ImageField(upload_to="images/%Y/%m%/%d")
+    description = models.CharField(max_length=255, blank=True)
+
--- views.py
+++ views.py
@@ -7,6 +7,9 @@ from django.core.urlresolvers import reverse_lazy as reverse
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 
+from django.contrib.auth.decoraters import login_required
+
+from comenius.models import Album
 # Create your views here.
 
 site = []
@@ -14,11 +17,18 @@ site = []
 site.append(nav("Home", reverse('comenius:index')))
 site.append(nav("Impressum", reverse('comenius:impressum')))
 
+album_nav = [
+    nav("Album Erstellen", reverse('comenius:album-create')),
+]
+
+site.append(nav("Album", subsites=album_nav))
+
 blog_extra = {
     "site": site,
     "appname": "Comenius Projekt",
     "homeurl": reverse('comenius:index'),
 }
+
 app_specific['comenius'] = blog_extra
 
 index = ExtraTemplateView.as_view(
@@ -43,6 +53,7 @@ about = ExtraTemplateView.as_view(
 		})
 
 login = lambda request: django.contrib.auth.views.login(request, extra_context=dict(blog_extra.items() + {'title':"Login"}.items()))
+
 logout = lambda request: django.contrib.auth.views.logout(request, extra_context=dict(blog_extra.items()))
 
 search = ExtraTemplateView.as_view(
@@ -52,3 +63,17 @@ search = ExtraTemplateView.as_view(
             'appname': 'comenius',
         })
 
+@login_required
+class AlbumCreate(ExtraCreateView):
+    model = Album
+    fields = ['name']
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super(AlbumCreate, self).form_valid(form)
+
+album_create = AlbumCreate.as_view(
+        extra = {
+            'title': "Album erstellen",
+            'appname': 'comenius',
+        })
